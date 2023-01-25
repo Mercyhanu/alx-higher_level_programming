f5debbc45574ea4034152c1dd31cc56d39a80ffd
Mercyhanu <mercyhanu2016@gmail.com>
2023-01-25 16:35:54 +0000

    square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+A simple program with only a class that describes
+a square
+"""
+
+
+class Square:
+    """
+    An empty class describing a square.
+    """
+    pass
