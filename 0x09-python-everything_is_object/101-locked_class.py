7d86a0cbcf0450b2281984e73f373ba677b09fdb
Mercyhanu <mercyhanu2016@gmail.com>
2023-02-01 18:22:15 +0000

    everythings

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Create a locked class
+"""
+
+
+class LockedClass:
+    """
+    A class that prevents the instanciation
+    of attributes except for first_name
+    """
+
+    __slots__ = ['first_name']
+
+    def __init__(self, first_name=''):
+        """
+        Initializes first_name
+        """
+
+        self.first_name = first_name
