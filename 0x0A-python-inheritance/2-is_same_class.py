70c990d4489671ae35858ef1977566ee24e5e6e3
Mercyhanu <mercyhanu2016@gmail.com>
2023-02-06 12:44:02 +0000

    inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A program to tell whether an object is
+or not an instance of another
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Determines if an object is exactly an
+    instance of another
+    """
+    return True if type(obj) is a_class else False
